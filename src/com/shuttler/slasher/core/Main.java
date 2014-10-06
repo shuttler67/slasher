16d5f65ab54a9aa60697639b0930f135519c6123
shuttler67 <shuttler67@gmail.com>
2014-10-06 12:02:29 +0200

    oops forgot the natives

--- src/com/shuttler/slasher/core/Main.java
+++ src/com/shuttler/slasher/core/Main.java
@@ -1,5 +1,12 @@
 package com.shuttler.slasher.core;
 
+import pixeltoy.PixelToy;
+
 public class Main {
 
+    public static void main(String[] args) {
+        PixelToy.init();
+
+        PixelToy.quit();
+    }
 }
