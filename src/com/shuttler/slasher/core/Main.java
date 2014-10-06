682205c0ef10e8b3263dd436390451cae30e18d6
shuttler67 <shuttler67@gmail.com>
2014-10-06 12:23:03 +0200

    fixed pixeltoy bug

--- src/com/shuttler/slasher/core/Main.java
+++ src/com/shuttler/slasher/core/Main.java
@@ -7,6 +7,8 @@ public class Main {
     public static void main(String[] args) {
         PixelToy.init();
 
-        PixelToy.quit();
+        while (true) {
+            PixelToy.newFrame();
+        }
     }
 }
